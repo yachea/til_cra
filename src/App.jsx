89a1c4560aadf5be5f83f78a04015b4919891a8d
yachea <dev.yachea@gmail.com>
2025-07-18 12:19:28 +0900

    [docs] useRef의 이해

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import IndexPage from "./pages/Index";
-import AboutPage from "./pages/about/About";
-import MissionPage from "./pages/about/Mission";
-import TeamPage from "./pages/about/Team";
-import ServicePage from "./pages/service/Service";
-import BlogPage from "./pages/blog/Blog";
-import DetailPage from "./pages/blog/Detail";
-import DesignPage from "./pages/blog/Design";
-import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React, { useRef } from "react";
 
 function App() {
-  // js 자리
-  const [isLogin, setIsLogin] = useState(false);
-  useEffect(() => {
-    console.log(isLogin);
-  }, [isLogin]);
-  // jsx 자리
+  const countRef = useRef(0);
+  const incre = () => {
+    countRef.current++;
+    console.log(countRef.current);
+  };
   return (
-    <Router>
-      <Header
-        company={"좋은 회사"}
-        service={"Todo 서비스"}
-        setIsLogin={setIsLogin}
-      >
-        <div>👩 나는 자식입니다.</div>
-        <div>👶 나는 자식은 여러명 가능합니다.</div>
-      </Header>
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={<IndexPage first={"첫 페이지 입니다."} />}
-          ></Route>
-          <Route path="/about">
-            <Route index element={<AboutPage />}></Route>
-            <Route path="mission" element={<MissionPage />}></Route>
-            <Route path="team" element={<TeamPage />}></Route>
-          </Route>
-          <Route path="/service" element={<ServicePage />}></Route>
-          <Route path="/blog">
-            <Route index element={<BlogPage />}></Route>
-            {/* 중첩 겹침 */}
-            <Route path="design">
-              <Route path=":id" element={<DesignPage />}></Route>
-              <Route path="detail" element={<DetailPage />}></Route>
-            </Route>
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer>
-        {isLogin ? <p>로그인 중이시네요.</p> : <p>로그아웃 중이시군요.</p>}
-      </Footer>
-    </Router>
+    <div>
+      <h1>값 보관 및 저장 {countRef.current}</h1>
+      <button onClick={incre}>증가</button>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ const Hello = function () {
 
 root.render(
   <div className="wrap">
-    <Index />
+    <App />
   </div>,
 );
